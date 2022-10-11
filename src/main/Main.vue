e46d5939fd70404b95ca16489b4177e646ab3476
Alexis B <alexis-contractor@playplay.com>
2022-10-11 11:37:08 +0200

    init poc of new project architecture

---
+++ src/main/Main.vue
@@ -0,0 +1,28 @@
+<template>
+  
+</template>
+
+<script lang="ts">
+import {defineComponent} from '@vue/composition-api';
+
+export default defineComponent({
+  name: "Studio",
+  components: {
+  },
+  props: {
+  },
+  setup(props) {
+    onMounted(() => {
+      const teamStore = useTeamStore()
+      const teamId = teamStore.teams.currentTeamId
+      registerStore('Branding') //branding is a shared module and now given us context to the page
+      registerStore('Studio') //studio is a scope module and now given us context as well
+      // polatateBrandingStore(teamId) // will populate the branding store
+    })
+  },
+});
+</script>
+
+<style lang="scss">
+
+</style>
---
+++ src/views/Studio/Studio.vue
@@ -0,0 +1,31 @@
+<template>
+  
+</template>
+
+<script lang="ts">
+import {defineComponent} from '@vue/composition-api';
+
+export default defineComponent({
+  name: "Studio",
+  components: {
+  },
+  props: {
+  },
+  setup(props) {
+    onMounted(() => {
+      const teamStore = useTeamStore()
+      const teamId = teamStore.teams.currentTeamId
+      registerStore('ProjectStudio') //branding is a shared module and now given us context to the page
+      registerStore('Studio') //studio is a scope module and now given us context as well
+      // polatateBrandingStore(teamId) // will populate the branding store
+    })
+    onUnmounted(() => {
+      unregisterStore('Studio') //studio is a scope module and now given us context as well
+    })
+  },
+});
+</script>
+
+<style lang="scss">
+
+</style>
